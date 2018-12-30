0b4d9806f42d41cb1a9f27541eed5dd91d2efb82
ksenousi <ksenousi@gmail.com>
2018-12-30 12:03:24 +0100

    modified print statements in main

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"container/list"
+	"fmt"
 )
 
 func main() {
 	textNode := text("My first Text Node!\n")
-	fmt.Printf(textNode.text)
+	println(textNode.text)
 
 	elemNode := elem("My first Element Node!\n", nil, list.New())
-	fmt.Printf(elemNode.elementData.tagName)
+	println(elemNode.elementData.tagName)
+
+	parser := Parser{0, []rune("汉Hello字")}
+	fmt.Printf("\n%t\n", parser.startsWith("汉"))
+	println(string(parser.consumeChar()))
+	println(string(parser.consumeChar()))
+	fmt.Printf("%t\n", parser.startsWith("ello"))
+	parser.consumeWhitespace()
+	println(parser.pos)
+	fmt.Printf("%t\n", parser.eof())
+
 }
